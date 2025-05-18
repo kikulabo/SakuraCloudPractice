c99ea05b9b188b255a8ad0a5da4f5f0cdf2ad544
kikulabo <kikuchi@x-tech5.co.jp>
2025-05-18 23:15:27 +0900

    feat: トレース機能を追加

--- my-express-app/index.js
+++ my-express-app/index.js
@@ -4,8 +4,18 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send('Hello from Node.js with Express on Sakura Cloud!\n');
+});
+
+// 意図的にエラーを発生させるパスを追加
+app.get('/testerror', (_req, res) => {
+  try {
+    throw new Error('This is a test error for Mackerel log monitoring!');
+  } catch (error) {
+    console.error(`Error on /testerror path: ${error.message}`);
+    res.status(500).send('An intentional error occurred on the server.');
+  }
 });
 
 app.listen(port, hostname, () => {
